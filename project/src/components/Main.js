64a7b990a0ab2b834f666eda707eab89aefaefab
UssCringe <110967038+UssCringe@users.noreply.github.com>
2022-11-01 15:30:14 +0100

    Add files via upload

---
+++ project/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Main() {
+    return (
+    <div className="main">
+        <h1>Fun facts about react</h1>
+        <ul className="list">
+            <li>Was released in 2013</li>
+            <li>Was originally created by John Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+        <img className="big-logo" src="../react-icon-large.png" />
+    </div>)
+}
+
---
+++ project/src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+    <nav>
+        <img src="../logo.png" />
+        <h1>ReactFacts</h1>
+        <div>React Course - Project 1</div>
+    </nav>)
+}
